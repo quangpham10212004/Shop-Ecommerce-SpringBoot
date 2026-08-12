a40cf36ae521553223ea0edd8f38f5a7c823a6f6
j0eloft <quangpham10212004@gmail.com>
2026-08-12 17:31:25 +0700

    feat(feature): keycloak applied

--- order-service/src/main/java/com/aiecommerce/order/client/impl/ProductClientImpl.java
+++ order-service/src/main/java/com/aiecommerce/order/client/impl/ProductClientImpl.java
@@ -20,7 +20,7 @@ public class ProductClientImpl implements ProductClient {
     public List<ProductDto> getProductByIds(ProductFilter productFilter) {
         BaseResponse<List<ProductDto>> response = webClientBuilder.build()
                 .post()
-                .uri( "http://localhost:8888/v1/products/search")
+                .uri( "http://product-service/v1/products/search")
                 .bodyValue(productFilter)
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<BaseResponse<List<ProductDto>>>() {
